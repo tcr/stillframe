cbfdb26787a4040d2a558587bcf4743dd5bcfeea
Tim Cameron Ryan <id@timryan.org>
2015-03-20 15:38:17 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var camera = require('camera-usb');
+var fs = require('fs');
+var myip = require('my-ip');
+
+var app = express();
+
+app.get('/', function (req, res) {
+	res.setHeader('content-type', 'image/jpeg');
+	camera.acquire(function loop (err, camera) {
+		camera.captureFrame('jpeg')
+		.on('end', function () {
+			camera.close();
+		})
+		.pipe(res)
+	})
+})
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', myip(null, false), port)
+})
